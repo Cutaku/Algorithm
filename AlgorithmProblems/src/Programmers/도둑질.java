f936770a2da3c99d315bb55913723164b9178580
Cutaku <mmzz0219@gmail.com>
2023-04-05 22:00:00 +0900

    2023/04/05 프로그래머스 1문제

---
+++ AlgorithmProblems/src/Programmers/도둑질.java
@@ -0,0 +1,22 @@
+package Programmers;
+
+class 도둑질 {
+    public int solution(int[] money) {
+
+        int n = money.length;
+
+        int[] dp1 = new int[n];
+        int[] dp2 = new int[n];
+
+        dp1[0] = money[0];
+        dp1[1] = money[0];
+        dp2[1] = money[1];
+
+        for (int i = 2; i < n; i++) {
+            dp1[i] = Math.max(dp1[i - 2] + money[i], dp1[i - 1]);
+            dp2[i] = Math.max(dp2[i - 2] + money[i], dp2[i - 1]);
+        }
+
+        return Math.max(dp1[n - 2], dp2[n - 1]);
+    }
+}
