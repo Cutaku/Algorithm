bdc04a46314da42833dd0582309ba20c8881b9f0
Cutaku <mmzz0219@gmail.com>
2023-06-17 23:39:23 +0900

    2023/06/17 백준 1문제

---
+++ AlgorithmProblems/src/LCS2_9252.java
@@ -0,0 +1,49 @@
+import java.io.*;
+import java.util.Stack;
+
+public class LCS2_9252 {
+    public static void main(String[] args) throws IOException{
+
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+
+        String str1 = br.readLine();
+        String str2 = br.readLine();
+
+        int n = str1.length();
+        int m = str2.length();
+
+        int[][] ans = new int[m + 1][n + 1];
+
+        for (int i = 1; i <= m; i++) {
+            for (int j = 1; j <= n; j++) {
+                if (str1.charAt(j - 1) == str2.charAt(i - 1)) {
+                    ans[i][j] = ans[i - 1][j - 1] + 1;
+                } else {
+                    ans[i][j] = Math.max(ans[i][j - 1], ans[i - 1][j]);
+                }
+            }
+        }
+
+        System.out.println(ans[m][n]);
+
+        Stack<Character> stack = new Stack<>();
+
+        int i = n;
+        int j = m;
+
+        while (i > 0 && j > 0) {
+            if (str1.charAt(i - 1) == str2.charAt(j - 1)) {
+                stack.add(str1.charAt(--i));
+                j--;
+            } else if (ans[j][i - 1] > ans[j - 1][i]) {
+                i--;
+            } else {
+                j--;
+            }
+        }
+
+        while (!stack.isEmpty()) bw.append(stack.pop());
+        bw.flush();
+    }
+}
