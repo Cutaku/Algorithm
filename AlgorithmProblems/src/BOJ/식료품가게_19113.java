4161b40567e314bb23dd9bf968b1e0fed084cc98
Cutaku <mmzz0219@gmail.com>
2024-01-01 17:44:56 +0900

    2024/01/01 SWEA 1문제

---
+++ AlgorithmProblems/src/BOJ/식료품가게_19113.java
@@ -0,0 +1,45 @@
+package BOJ;
+
+import java.io.*;
+import java.util.Arrays;
+
+public class 식료품가게_19113 {
+    public static void main(String[] args) throws IOException {
+
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+
+        int T = Integer.parseInt(br.readLine());
+
+        for (int t = 1; t <= T; t++) {
+            bw.append("#").append(String.valueOf(t)).append(" ");
+
+            int n = Integer.parseInt(br.readLine());
+
+            int[] prices = Arrays.stream(br.readLine().split(" ")).mapToInt(Integer::parseInt).toArray();
+
+            boolean[] checked = new boolean[2 * n];
+
+            for (int i = 0; i < 2 * n; i++) {
+                if (checked[i]) continue;
+
+                checked[i] = true;
+
+                bw.append(String.valueOf(prices[i])).append(" ");
+
+                int price = prices[i] / 3 * 4;
+
+                for (int j = i + 1; j < 2 * n; j++) {
+                    if (prices[j] == price && !checked[j]) {
+                        checked[j] = true;
+                        break;
+                    }
+                }
+            }
+
+            bw.append("\n");
+        }
+
+        bw.flush();
+    }
+}
