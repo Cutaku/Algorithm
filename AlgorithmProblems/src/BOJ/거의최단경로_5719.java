8dcb3bb802d0c66369f42db16f284c443986644f
Cutaku <mmzz0219@gmail.com>
2024-01-25 08:18:31 +0900

    패키지 이동 및 코드 수정

--- AlgorithmProblems/src/BOJ/거의최단경로_5719.java
+++ AlgorithmProblems/src/BOJ/거의최단경로_5719.java
@@ -1,3 +1,5 @@
+package BOJ;
+
 import java.io.*;
 import java.util.*;
 
--- AlgorithmProblems/src/BOJ/파이프옮기기1_17070.java
+++ AlgorithmProblems/src/BOJ/파이프옮기기1_17070.java
@@ -6,34 +6,30 @@ import java.io.InputStreamReader;
 import java.util.Arrays;
 
 public class 파이프옮기기1_17070 {
-    public static void main(String[] args) throws IOException{
+    public static void main(String[] args) throws IOException {
 
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 
         int n = Integer.parseInt(br.readLine());
 
         int[][] home = new int[n][];
-        for (int i = 0; i < n; i++) home[i] = Arrays.stream(br.readLine().split(" ")).mapToInt(Integer::parseInt).toArray();
-
-        int[][] hori = new int[n][];
-        int[][] vert = new int[n][];
-        int[][] diag = new int[n][];
-
         for (int i = 0; i < n; i++) {
-            hori[i] = new int[n];
-            vert[i] = new int[n];
-            diag[i] = new int[n];
+            home[i] = Arrays.stream(br.readLine().split(" ")).mapToInt(Integer::parseInt).toArray();
         }
+
+        int[][] hori = new int[n][n];
+        int[][] vert = new int[n][n];
+        int[][] diag = new int[n][n];
 
         hori[0][1] = 1;
 
         for (int i = 0; i < n; i++) {
             for (int j = 0; j < n; j++) {
-                if (home[i][j] == 0) {
-                    if (j > 0) hori[i][j] += hori[i][j - 1] + diag[i][j - 1];
-                    if (i > 0) vert[i][j] += vert[i - 1][j] + diag[i - 1][j];
-                    if (i > 0 && j > 0 && home[i - 1][j] == 0 && home[i][j - 1] == 0) diag[i][j] += hori[i - 1][j - 1] + vert[i - 1][j - 1] + diag[i - 1][j - 1];
-                }
+                if (home[i][j] == 1) continue;
+
+                if (j > 0) hori[i][j] += hori[i][j - 1] + diag[i][j - 1];
+                if (i > 0) vert[i][j] += vert[i - 1][j] + diag[i - 1][j];
+                if (i > 0 && j > 0 && home[i - 1][j] == 0 && home[i][j - 1] == 0) diag[i][j] += diag[i - 1][j - 1] + hori[i - 1][j - 1] + vert[i - 1][j - 1];
             }
         }
 
