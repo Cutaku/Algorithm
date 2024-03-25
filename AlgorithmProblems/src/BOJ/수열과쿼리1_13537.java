ae38a85e941c9835cb253af268ca23455eda57b8
류인환 <mmzz0219@gmail.com>
2024-03-25 23:51:16 +0900

    2024/03/25 백준 1문제

---
+++ AlgorithmProblems/src/BOJ/수열과쿼리1_13537.java
@@ -0,0 +1,107 @@
+package BOJ;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
+
+public class 수열과쿼리1_13537 {
+    static Node[] seg;
+    static StringTokenizer st;
+
+    public static void main(String[] args) throws IOException {
+
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringBuilder sb = new StringBuilder();
+
+        int n = Integer.parseInt(br.readLine());
+
+        seg = new Node[n << 2];
+
+        st = new StringTokenizer(br.readLine());
+
+        init(1, 0, n - 1);
+
+        int m = Integer.parseInt(br.readLine());
+
+        for (int t = 0; t < m; t++) {
+            st = new StringTokenizer(br.readLine());
+
+            int i = Integer.parseInt(st.nextToken()) - 1;
+            int j = Integer.parseInt(st.nextToken()) - 1;
+            int k = Integer.parseInt(st.nextToken());
+
+            sb.append(countOverK(1, 0, n - 1, i, j, k)).append("\n");
+        }
+
+        System.out.println(sb);
+    }
+
+    static int countOverK(int idx, int left, int right, int i, int j, int k) {
+
+        if (right < i || j < left) return 0;
+
+        if (i <= left && right <= j) {
+            return seg[idx].countOverK(k);
+        }
+
+        int mid = (left + right) >> 1;
+
+        return countOverK(idx << 1, left, mid, i, j, k) + countOverK(idx << 1 | 1, mid + 1, right, i, j, k);
+    }
+
+    static Node init(int idx, int left, int right) {
+        if (left == right) {
+            return seg[idx] = new Node(Integer.parseInt(st.nextToken()));
+        }
+
+        int mid = (left + right) >> 1;
+
+        return seg[idx] = new Node(init(idx << 1, left, mid), init(idx << 1 | 1, mid + 1, right));
+    }
+
+    static class Node {
+        int[] nums;
+
+        Node(int n) {
+            nums = new int[]{n};
+        }
+
+        Node(Node c1, Node c2) {
+            nums = new int[c1.nums.length + c2.nums.length];
+
+            int i = 0, j = 0;
+            int k = 0;
+
+            while (i < c1.nums.length || j < c2.nums.length) {
+                if (i == c1.nums.length) {
+                    nums[k++] = c2.nums[j++];
+                } else if (j == c2.nums.length) {
+                    nums[k++] = c1.nums[i++];
+                } else {
+                    if (c1.nums[i] < c2.nums[j]) {
+                        nums[k++] = c1.nums[i++];
+                    } else {
+                        nums[k++] = c2.nums[j++];
+                    }
+                }
+            }
+        }
+
+        int countOverK(int k) {
+            if (nums[0] > k) return nums.length;
+            if (nums[nums.length - 1] <= k) return 0;
+
+            int s = 0, e = nums.length - 1;
+
+            while (e - s > 1) {
+                int m = (s + e) >> 1;
+
+                if (nums[m] <= k) s = m;
+                else e = m;
+            }
+
+            return nums.length - e;
+        }
+    }
+}
