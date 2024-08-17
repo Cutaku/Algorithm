4175e35856e25aca57d24621e0f30592cbb6ec49
류인환 <mmzz0219@gmail.com>
2024-08-17 14:26:00 +0900

    2024/08/16 백준 1문제

---
+++ AlgorithmProblems/src/BOJ/배열과연산_14222.java
@@ -0,0 +1,32 @@
+package BOJ;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.PriorityQueue;
+
+public class 배열과연산_14222 {
+    public static void main(String[] args) throws IOException {
+
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+
+        int n = Integer.parseInt(br.readLine());
+
+        PriorityQueue<Integer> pq  = new PriorityQueue<>();
+
+        for (int i = 0; i < n; i++) pq.add(Integer.parseInt(br.readLine()));
+
+        int c = 1;
+        long ans = 0;
+
+        while (!pq.isEmpty()) {
+            int poll = pq.poll();
+
+            if (c > poll) continue;
+
+            ans += poll - c++;
+        }
+
+        System.out.println(ans);
+    }
+}
