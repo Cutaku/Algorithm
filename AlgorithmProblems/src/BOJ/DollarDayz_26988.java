102741d4120fa0e8315d60504a5949f7e1c15537
류인환 <mmzz0219@gmail.com>
2025-04-01 11:21:54 +0900

    2025/04/01 백준 1문제

---
+++ AlgorithmProblems/src/BOJ/DollarDayz_26988.java
@@ -0,0 +1,30 @@
+package BOJ;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.math.BigInteger;
+import java.util.StringTokenizer;
+
+public class DollarDayz_26988 {
+    public static void main(String[] args) throws IOException {
+
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+
+        StringTokenizer st = new StringTokenizer(br.readLine());
+        int n = Integer.parseInt(st.nextToken()), k = Integer.parseInt(st.nextToken());
+
+        BigInteger[] dp = new BigInteger[n + 1];
+        for (int i = 0; i <= n; i++) dp[i] = BigInteger.ONE;
+
+        for (int i = 2; i <= k; i++) {
+            for (int j = n; j >= i; j--) {
+                for (int l = 1; j >= i * l ; l++) {
+                    dp[j] = dp[j].add(dp[j - i * l]);
+                }
+            }
+        }
+
+        System.out.println(dp[n]);
+    }
+}
